bceb46f70e1569d5ab3bc27e85fa5c93511fbcd7
段昊辰 <duanhaochen@126.com>
2019-08-22 23:31:15 +0800

    feat: home添加banner

--- lib/home/main.dart
+++ lib/home/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home.'),
+    return Scaffold(
+      body: Container(
+        color: Color(0xffffffff),
+        child: Column(
+          children: <Widget>[
+            // 图片banner
+            Row(children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Image.asset('assets/images/banner.jpg'),
+                ),
+              )
+            ],),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/login/main.dart
+++ lib/login/main.dart
@@ -5,17 +5,19 @@ import './form.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xffffffff),
-      child: Column(children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 54.0, bottom: 24.0),
-          child: Center(
-            child: Text('Flutter Demo', style: TextStyle(fontSize: 24, color: Color(0xff2f54eb))),
+    return Scaffold(
+      body: Container(
+        color: Color(0xffffffff),
+        child: Column(children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 54.0, bottom: 24.0),
+            child: Center(
+              child: Text('Flutter Demo', style: TextStyle(fontSize: 24, color: Color(0xff2f54eb))),
+            ),
           ),
-        ),
-        LoginForm(),
-      ])
+          LoginForm(),
+        ])
+      ),
     );
   }
 }
